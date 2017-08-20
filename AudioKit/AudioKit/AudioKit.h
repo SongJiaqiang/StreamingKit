abc6b80e8a3c97f7cd1f826ca313071e035578f3
Jarvis <sjq2150@gmail.com>
2017-08-20 18:01:07 +0800

    添加AudioKit.framework

---
+++ AudioKit/AudioKit/AudioKit.h
@@ -0,0 +1,21 @@
+//
+//  AudioKit.h
+//  AudioKit
+//
+//  Created by Jarvis on 20/08/2017.
+//  Copyright © 2017 JQTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STKAudioPlayer.h"
+
+//! Project version number for AudioKit.
+FOUNDATION_EXPORT double AudioKitVersionNumber;
+
+//! Project version string for AudioKit.
+FOUNDATION_EXPORT const unsigned char AudioKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioKit/PublicHeader.h>
+
+
---
+++ AudioKit/AudioKitTests/AudioKitTests.m
@@ -0,0 +1,39 @@
+//
+//  AudioKitTests.m
+//  AudioKitTests
+//
+//  Created by Jarvis on 20/08/2017.
+//  Copyright © 2017 JQTech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AudioKitTests : XCTestCase
+
+@end
+
+@implementation AudioKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
